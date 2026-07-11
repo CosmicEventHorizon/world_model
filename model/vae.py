281b64929a99a2e7084e4bd84be5a46887f25e4b
CosmicEventHorizon <bloodpirouette@gmail.com>
2026-07-11 16:36:52 -0400

    vae

--- model/vae.py
+++ model/vae.py
@@ -4,13 +4,15 @@ import torch.nn as nn
 
 class Encoder(nn.Module):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=1, kernel_size=4, stride=2),
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=4, stride=2),
             nn.ReLU(),
-            nn.Conv2d(in_channels=1, out_channels=1, kernel_size=4, stride=2),
-            nn.RELU(),
-            nn.Conv2d(in_channels=1, out_channels=1, kernel_size=4, stride=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=2),
             nn.ReLU(),
         )
         self.fully_connected_mean = nn.Sequential(
@@ -24,9 +26,49 @@ class Encoder(nn.Module):
     def forward(self, x):
         x = self.conv(x)
         x = torch.flatten(x)
-        self.z_mean = self.fully_connected(x)
-        self.z_variance = self.fully_connected_mean(x)
+        z_mean = self.fully_connected_mean(x)
+        z_variance = self.fully_connected_variance(x)
+        return (z_mean, z_variance)
 
 
-if __name__ == "__main__":
-    print(torch.cuda.is_available())
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.deconv = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=1024, out_channels=128, kernel_size=5, stride=2
+            ),
+            nn.ReLU(),
+            nn.ConvTranspose2d(
+                in_channels=128, out_channels=64, kernel_size=5, stride=2
+            ),
+            nn.ReLU(),
+            nn.ConvTranspose2d(
+                in_channels=64, out_channels=32, kernel_size=6, stride=2
+            ),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=6, stride=2),
+        )
+
+    def forward(self, x):
+        return self.deconv(x)
+
+
+class VAE(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def train(self, x):
+        (z_mean, z_variance) = self.encoder(x)
+        z_sample = torch.normal(z_mean, z_variance * torch.eye(1024, 1))
+        x_hat = self.decoder(z_sample)
+        kl_loss = nn.KLDivLoss()
+        loss = (
+            torch.sub(x, x_hat).norm + kl_loss(z_sample, torch.normal(torch.zeros()))
+        ).sum()
+        loss.backward()
+        optim = torch.optim.SGD(self.parameters(), lr=1e-2, momentum=0.9)
+        optim.step()
+        torch.save(self.state_dict(), "model.bin")
