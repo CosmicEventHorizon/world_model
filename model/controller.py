a4dbbce1bd6ed06bab6ad8409f6b3c8ff7e949ce
CosmicEventHorizon <bloodpirouette@gmail.com>
2026-07-29 19:02:32 -0400

    controller complete

--- model/controller.py
+++ model/controller.py
@@ -6,7 +6,7 @@ class Controller(nn.Module):
     def __init__(self):
         super().__init__()
         self.fully_connected = nn.Sequential(
-            nn.Linear(32, 64), nn.LeakyReLU(), nn.Linear(64, 32), nn.LeakyReLU()
+            nn.Linear(35, 64), nn.LeakyReLU(), nn.Linear(64, 32), nn.LeakyReLU()
         )
         self.fc_steer = nn.Sequential(nn.Linear(32, 1), nn.Tanh())
         self.fc_accelerate = nn.Sequential(nn.Linear(32, 1), nn.Sigmoid())
--- training/controller_train.py
+++ training/controller_train.py
@@ -15,27 +15,38 @@ def current_directory_path(filename):
     return os.path.join(CURRENT_DIRECTORY, filename)
 
 
-def drive_n_times(n, mean):
-    LEARNING_RATE = 0.0001
+def drive_n_times(n, mean, vae, c):
+    LEARNING_RATE = 0.005
     PARAMETER_SD = 0.05
-    rewards = []
+    positive_rewards = []
+    negative_rewards = []
     noises = []
     for sample in range(n):
         noise = torch.randn_like(ut.parameters_to_vector(c.parameters()))
         noises.append(noise)
-        sampled_c_parameters = mean + PARAMETER_SD * noise
-        ut.vector_to_parameters(sampled_c_parameters, c.parameters())
-        reward = start_driving(env, vae, c)
-        rewards.append(reward)
-        print(f"Drove {sample+1}/{n} times. Reward: {reward}")
+        positive_sampled_c_parameters = mean + PARAMETER_SD * noise
+        ut.vector_to_parameters(positive_sampled_c_parameters, c.parameters())
+        positive_reward = start_driving(vae, c)
+        positive_rewards.append(positive_reward)
 
-    update = calculate_update(rewards, noises)
+        negative_sampled_c_parameters = mean - PARAMETER_SD * noise
+        ut.vector_to_parameters(negative_sampled_c_parameters, c.parameters())
+        negative_reward = start_driving(vae, c)
+        negative_rewards.append(negative_reward)
+
+        print(
+            f"Drove {sample+1}/{n} times. Positive Reward: {positive_reward}. Negative Reward: {negative_reward}"
+        )
+
+    update = calculate_update(positive_rewards, negative_rewards, noises)
     mean = mean + (LEARNING_RATE / (PARAMETER_SD * n)) * update
+    rewards = positive_rewards + negative_rewards
     average_reward = sum(rewards) / len(rewards)
     return mean, average_reward
 
 
-def start_driving(env, vae, c):
+def start_driving(vae, c, render_mode="rgb_array"):
+    env = load_gym(render_mode)
     observation, info = env.reset()
     episode_over = False
     reward_accum = 0
@@ -43,19 +54,17 @@ def start_driving(env, vae, c):
     while not episode_over:
         observation = hwc_to_chw(observation).to(device)
         z = vae.encode(observation)
-        action = generate_action(z, c)
+        hint = get_hint(env, device)
+        action = generate_action(z, hint, c)
         observation, reward, terminated, truncated, info = env.step(action)
         reward_accum += reward
         episode_over = terminated or truncated
+    env.close()
     return reward_accum
 
 
 def preview_model(vae, c):
-    env = load_gym("human")
-    c.load_state_dict(
-        torch.load(current_directory_path("c_model.bin"), map_location=device)
-    )
-    start_driving(env, vae, c)
+    start_driving(vae, c, "human")
 
 
 def load_vae(device) -> VAE:
@@ -77,8 +86,19 @@ def hwc_to_chw(x):
     return torch.as_tensor(x).permute(2, 0, 1).unsqueeze(0).float() / 255.0
 
 
-def generate_action(z, c):
-    steer, accelerate, brake = c.forward(z)
+def get_hint(env, device):
+    linear_velocity = env.unwrapped.car.hull.linearVelocity
+    angular_velocity = env.unwrapped.car.hull.angularVelocity
+    return torch.tensor(
+        [linear_velocity.x, linear_velocity.y, angular_velocity],
+        dtype=torch.float32,
+        device=device,
+    )
+
+
+def generate_action(z, hint, c):
+    z_with_hint = torch.cat((z, hint), dim=0)
+    steer, accelerate, brake = c.forward(z_with_hint)
     return np.array(
         [
             steer[0].detach().cpu().item(),
@@ -89,40 +109,58 @@ def generate_action(z, c):
     )
 
 
-def calculate_update(rewards, noises):
-    rewards_mean = np.mean(rewards)
+def calculate_update(positive_rewards, negative_rewards, noises):
+    rewards = positive_rewards + negative_rewards
     rewards_std = np.std(rewards) + 1e-8
-    length = len(rewards)
+
+    length = len(positive_rewards)
     sum = 0
     for i in range(length):
-        standardized_reward = (rewards[i] - rewards_mean) / rewards_std
-        sum += standardized_reward * noises[i]
+        standardized_reward_difference = (
+            positive_rewards[i] - negative_rewards[i]
+        ) / rewards_std
+        sum += standardized_reward_difference * noises[i]
     return sum
 
 
 if __name__ == "__main__":
-    EPOCH = 200
+    EPOCH = 1000
     NO_PARAMETERS_SAMPLES = 16
 
-    env = load_gym("rgb_array")
     device = torch.device("cuda")
     vae = load_vae(device)
     c = Controller().to(device)
     if os.path.exists(current_directory_path("c_model.bin")):
-        preview_model(vae, c)
-        quit()
+        answer = input(
+            "c_model.bin exists. Continue training from saved parameters? [y/n]: "
+        )
+        if answer.lower() == "y":
+            c.load_state_dict(
+                torch.load(current_directory_path("c_model.bin"), map_location=device)
+            )
+        else:
+            c.load_state_dict(
+                torch.load(current_directory_path("c_model.bin"), map_location=device)
+            )
+            preview_model(vae, c)
+            quit()
+
     mean = ut.parameters_to_vector(c.parameters()).clone()
 
     print("Started training...")
     max_reward = float("-inf")
     for epoch in range(EPOCH):
-        mean, average_reward = drive_n_times(NO_PARAMETERS_SAMPLES, mean)
-        if average_reward > max_reward:
-            max_reward = average_reward
+        mean, average_reward = drive_n_times(NO_PARAMETERS_SAMPLES, mean, vae, c)
+
         ut.vector_to_parameters(mean, c.parameters())
-        torch.save(c.state_dict(), current_directory_path("c_model.bin"))
+        mean_reward = start_driving(vae, c)
+
+        if mean_reward > max_reward:
+            max_reward = mean_reward
+            torch.save(c.state_dict(), current_directory_path("c_model.bin"))
+
         print("**************************************************************\n")
         print(
-            f"EPOCH: {epoch+1}/{EPOCH}. Average Reward: {average_reward}. Maximum Reward: {max_reward}"
+            f"EPOCH: {epoch+1}/{EPOCH}. Average Reward: {average_reward}. Mean Reward: {mean_reward}. Maximum Reward: {max_reward}"
         )
         print("\n**************************************************************")
